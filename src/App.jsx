656c395593b3cb5a441ff36ddc70d01d0a04d4b3
LeandroReichardt <163359722+LeandroReichardt@users.noreply.github.com>
2024-12-11 00:09:34 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ListadeContatos from './components/ListadeContatos'
+
+function App() {
+
+  return (
+    <>
+    <ListadeContatos/>
+    </>
+  )
+}
+
+export default App
